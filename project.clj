b95ae0e3a4d4064629a31b1c662002d286e4d4eb
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-07-13 19:05:54 +0200

    Speclj added to the project

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:dependencies [[speclj "3.0.2"]
+                                  [speclj-notify-osd "0.0.2"]]
+                   :plugins [[speclj "3.0.2"]]
+                   :test-paths ["spec"]}}
   :eval-in-leiningen true)
---
+++ spec/leiningen/documentation_spec.clj
@@ -0,0 +1,7 @@
+(ns leiningen.documentation-spec
+  (:require [speclj.core :refer :all]
+            [leiningen.documentation :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 1 1)))
