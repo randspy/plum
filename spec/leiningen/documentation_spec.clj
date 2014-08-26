60e8cb0c03ed986498e172bc566fa89e1b7cbb84
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-08-26 18:59:52 +0200

    Started processing test content

--- spec/leiningen/documentation_spec.clj
+++ spec/leiningen/documentation_spec.clj
@@ -1,7 +1,38 @@
 (ns leiningen.documentation-spec
   (:require [speclj.core :refer :all]
-            [leiningen.documentation :refer :all]))
+            [leiningen.documentation :refer :all]
+            [leiningen.debug :refer :all]
+            [clojure.zip :as zip]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 1 1)))
+
+(def doc-marker "PlumMarker:")
+
+(describe "Gets a function name."
+          (it "Name exist."
+              (should= "name" (function-name-after-marker
+                                 (str doc-marker "name desc") doc-marker)))
+          (it "Name exist with a space."
+              (should= "" (function-name-after-marker
+                                 (str doc-marker " name") doc-marker)))
+          (it "Name is a last string present."
+              (should= "name" (function-name-after-marker
+                                 (str doc-marker "name ") doc-marker)))
+          (it "Name does not exist."
+              (should= "" (function-name-after-marker
+                                 doc-marker doc-marker))))
+
+(describe "Gets test block without a function name and a marker."
+          (describe "Nothing will be removed"
+                    (it "No marker and a function name."
+                        (should= "(describe \"text\")"
+                                 (extract-text  "(describe \"text\")" doc-marker))))
+          (it "Marker with a name will be removed."
+              (should= "(describe \"text\")"
+                       (extract-text  (str "(describe \"" doc-marker "name text\")") doc-marker)))
+          (it "Marker will be removed."
+              (should= "(describe \" \")"
+                       (extract-text (str "(describe \"" doc-marker " \")" ) doc-marker)))
+          (describe "Convention is not respected."
+                    (it "Marker will be removed."
+                        (should= "" (extract-text (str "(describe \"" doc-marker "\")" ) doc-marker)))))
+
--- src/leiningen/documentation.clj
+++ src/leiningen/documentation.clj
@@ -1,1 +1,23 @@
 (ns leiningen.documentation)
+
+(defn function-name-after-marker [text marker]
+  (let [name-start-position (+ (count marker) (.indexOf text marker))
+        after-marker (subs text name-start-position)]
+    (first (clojure.string/split (subs text name-start-position) #" "))))
+
+(defn extract-text
+  "Removes marker and name from the input text, if there is no marker returns
+   the original text"
+  [text marker]
+  (if (.contains text marker)
+    (let [name (function-name-after-marker text marker)
+          space-between-name-and-text (if (zero? (count name)) 0 1)
+          marker-begin-index (.indexOf text marker)
+          marker-after-index (+ marker-begin-index
+                                (count marker)
+                                (count name)
+                                space-between-name-and-text)]
+      (try
+        (str (subs text 0 marker-begin-index) (subs text marker-after-index))
+        (catch StringIndexOutOfBoundsException e "")))
+    text))
